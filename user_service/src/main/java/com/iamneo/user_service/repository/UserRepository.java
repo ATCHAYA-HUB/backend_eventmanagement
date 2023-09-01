fcc7222f8dbde8c8ce14b9cc23b0e8fdd4e7b240
ATCHAYA-HUB <113664671+ATCHAYA-HUB@users.noreply.github.com>
2023-09-01 10:15:01 +0530

    Delete user_service directory

--- user_service/src/main/java/com/iamneo/user_service/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iamneo.user_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.iamneo.user_service.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
